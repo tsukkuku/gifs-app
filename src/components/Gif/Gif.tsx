903b1d74f5207c2f31a5ff3cc15f2aac33011b5d
tsukkuku <bebracs@bk.ru>
2025-06-08 20:17:51 +0300

    feat: add variants for Gif component

--- src/components/Gif/Gif.tsx
+++ src/components/Gif/Gif.tsx
@@ -1,13 +1,17 @@
 import type { FC } from "react";
 import style from "./Gif.module.scss";
+import cn from "classnames";
+
+type GifVariant = "default" | "random";
 
 interface GifProps {
   src: string;
   title: string;
+  variant?: GifVariant;
 }
 
-const Gif: FC<GifProps> = ({ src, title }) => {
-  return <img src={src} alt={title} className={style.img} />;
+const Gif: FC<GifProps> = ({ src, title, variant = "default" }) => {
+  return <img src={src} alt={title} className={cn(style[variant])} />;
 };
 
 export default Gif;
