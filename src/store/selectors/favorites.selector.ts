30493b71efed864cb2a1dffc83f248e09ff7a1ea
tsukkuku <bebracs@bk.ru>
2025-06-11 22:49:34 +0300

    refactor: change name to the correct

--- src/store/selectors/favorites.selector.ts
+++ src/store/selectors/favorites.selector.ts
@@ -2,5 +2,5 @@ import type { RootState } from "../store";
 
 export const selectAllFavorites = (state: RootState) => state.favorite.items;
 
-export const selectPressedStauts = (id: string) => (state: RootState) =>
+export const selectPressedStatus = (id: string) => (state: RootState) =>
   state.favorite.items.find((item) => item.id === id);
