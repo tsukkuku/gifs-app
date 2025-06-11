9b77f1ae51555910d3d9c12e97748bb04c53ed18
tsukkuku <bebracs@bk.ru>
2025-06-11 19:25:40 +0300

    feat: add new props for icons in button

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -6,17 +6,23 @@ type ButtonVariant = "primary" | "pagination" | "danger";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: ButtonVariant;
+  startContent?: string;
+  endContent?: string;
 }
 
 const Button: FC<ButtonProps> = ({
   children,
   variant = "primary",
   onClick,
+  startContent,
+  endContent,
   ...props
 }) => {
   return (
     <button onClick={onClick} className={clsx(style[variant])} {...props}>
+      {startContent && <img src={startContent} alt="icon" />}
       {children}
+      {endContent && <img src={endContent} alt="icon" />}
     </button>
   );
 };
