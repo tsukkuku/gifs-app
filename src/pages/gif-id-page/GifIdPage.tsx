6990efe0aa386b60e5eff5a00a0cfb1d6498bf7c
tsukkuku <bebracs@bk.ru>
2025-06-13 00:36:40 +0300

    refactor: removed repeats in code

--- src/pages/gif-id-page/GifIdPage.tsx
+++ src/pages/gif-id-page/GifIdPage.tsx
@@ -11,41 +11,45 @@ import style from "./GidIdPage.module.scss";
 import { useState } from "react";
 
 const GifIdPage = () => {
-  const [isCopied, setIsCopied] = useState<boolean>(false);
   const dispatch = useAppDispatch();
   const { id } = useParams<{ id: string }>();
   const { data, isLoading } = useGetGifByIdQuery(id || "", { skip: !id });
-  const isPressed = useAppSelector(selectPressedStatus(data?.data.id || ""));
+  const gifData = data?.data;
+  const gifUrl = gifData?.images.original.url;
+  const user = gifData?.user;
+  const isPressed = useAppSelector(selectPressedStatus(gifData?.id || ""));
+  const [isCopied, setIsCopied] = useState<boolean>(false);
 
   const handleCopy = () => {
-    navigator.clipboard.writeText(data?.data.images.original.url || "");
+    navigator.clipboard.writeText(gifUrl || "");
     toast.success("Link is copied!");
     setIsCopied(true);
     setTimeout(() => setIsCopied(false), 2100);
   };
 
   const handleDownload = () => {
-    window.open(data?.data?.images?.original?.url, "_blank");
+    window.open(gifUrl, "_blank");
   };
 
   if (isLoading) return <ClipLoader color="white" size={64} />;
-  if (!data?.data?.images?.original?.url) return <div>GIF not found</div>;
+  if (!gifUrl) return <div>GIF not found</div>;
+
   return (
     <div className={style.gifID}>
       <div className={style.textContent}>
         <div className={style.infoContainer}>
           <img
             src={
-              data?.data?.user?.avatar_url ||
+              user?.avatar_url ||
               "https://cdn-icons-png.flaticon.com/512/16522/16522939.png"
             }
             alt="USER AVATAR"
           />
           <div className={style.info}>
-            <h2>{data?.data?.user?.display_name || "Anonim"}</h2>
-            <a href={data?.data?.user?.profile_url} target="_blank">
-              @{data?.data?.user?.username || "Anonim"}{" "}
-              {data?.data?.user?.is_verified && (
+            <h2>{user?.display_name || "Anonim"}</h2>
+            <a href={user?.profile_url} target="_blank">
+              @{user?.username || "Anonim"}
+              {user?.is_verified && (
                 <img
                   style={{ width: "15px", height: "15px" }}
                   src="https://cdn-icons-png.flaticon.com/512/1828/1828640.png"
@@ -54,7 +58,7 @@ const GifIdPage = () => {
               )}
             </a>
             <div className={style.desc}>
-              <p>{data?.data?.user?.description || "No description."}</p>
+              <p>{user?.description || "No description."}</p>
             </div>
           </div>
         </div>
@@ -65,7 +69,7 @@ const GifIdPage = () => {
                 ? `https://img.icons8.com/?size=100&id=10287&format=png&color=F20303`
                 : "https://img.icons8.com/?size=100&id=10287&format=png&color=FFFFFF"
             }
-            onClick={() => dispatch(addFavorite(data.data))}
+            onClick={() => dispatch(addFavorite(gifData))}
           >
             {isPressed ? "Removed" : "Favorite"}
           </Button>
@@ -83,13 +87,14 @@ const GifIdPage = () => {
           </Button>
         </div>
       </div>
-      <Gif item={data.data} variant="random" />
+      <Gif item={gifData} variant="random" />
       <ToastContainer
         position="bottom-right"
         limit={1}
         theme="dark"
         autoClose={1500}
         closeOnClick
+        pauseOnHover={false}
       />
     </div>
   );
