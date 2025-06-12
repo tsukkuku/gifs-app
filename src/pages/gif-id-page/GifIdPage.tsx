f9284ecf0ab8b26767ba1095b8ba61154c7f8afd
tsukkuku <bebracs@bk.ru>
2025-06-12 21:05:25 +0300

    feat: add GifIDPage

---
+++ src/pages/gif-id-page/GifIdPage.tsx
@@ -0,0 +1,87 @@
+import { useGetGifByIdQuery } from "@/services/api";
+import { useParams } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
+import Button from "@/components/ui/Button/Button";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { addFavorite } from "@/store/slices/favorites.slice";
+import Gif from "@/components/Gif/Gif";
+import { selectPressedStatus } from "@/store/selectors/favorites.selector";
+import { toast, ToastContainer } from "react-toastify";
+import style from "./GidIdPage.module.scss";
+import { useState } from "react";
+
+const GifIdPage = () => {
+  const [isCopied, setIsCopied] = useState<boolean>(false);
+  const dispatch = useAppDispatch();
+  const { id } = useParams<{ id: string }>();
+  const { data, isLoading } = useGetGifByIdQuery(id || "", { skip: !id });
+  const isPressed = useAppSelector(selectPressedStatus(data?.data.id || ""));
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(data?.data.images.original.url || "");
+    toast.success("Link is copied!");
+    setIsCopied(true);
+    setTimeout(() => setIsCopied(false), 2000);
+  };
+
+  if (isLoading) return <ClipLoader color="white" size={64} />;
+  if (!data?.data?.images?.original?.url) return <div>GIF not found</div>;
+  return (
+    <div className={style.gifID}>
+      <div className={style.textContent}>
+        <div className={style.infoContainer}>
+          <img
+            src={
+              data?.data?.user?.avatar_url ||
+              "https://cdn-icons-png.flaticon.com/512/16522/16522939.png"
+            }
+            alt="USER AVATAR"
+          />
+          <div className={style.info}>
+            <h2>{data?.data?.user?.display_name || "Anonim"}</h2>
+            <a href={data?.data?.user?.profile_url} target="_blank">
+              @{data?.data?.user?.username || "Anonim"}{" "}
+              <img
+                style={{ width: "15px", height: "15px" }}
+                src="https://cdn-icons-png.flaticon.com/512/1828/1828640.png"
+                alt="icon"
+              />
+            </a>
+            <p>{data.data.title}</p>
+          </div>
+        </div>
+        <div className={style.buttons}>
+          <Button
+            startContent={
+              isPressed
+                ? `https://img.icons8.com/?size=100&id=10287&format=png&color=F20303`
+                : "https://img.icons8.com/?size=100&id=10287&format=png&color=FFFFFF"
+            }
+            onClick={() => dispatch(addFavorite(data.data))}
+          >
+            Favorite
+          </Button>
+          <Button
+            startContent="https://img.icons8.com/?size=100&id=11322&format=png&color=FFFFFF"
+            onClick={handleCopy}
+          >
+            {isCopied ? "Copied!" : "Copy Link"}
+          </Button>
+          <Button startContent="https://img.icons8.com/?size=100&id=vk3CfSUT5UA6&format=png&color=FFFFFF">
+            <a href={data.data.images.original.url}>Download</a>
+          </Button>
+        </div>
+      </div>
+      <Gif item={data.data} variant="random" />
+      <ToastContainer
+        position="bottom-right"
+        limit={1}
+        theme="dark"
+        autoClose={1500}
+        closeOnClick
+      />
+    </div>
+  );
+};
+
+export default GifIdPage;
