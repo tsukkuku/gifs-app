c16af25f3743fa75739c804da7dcd6d71c4d5be2
tsukkuku <bebracs@bk.ru>
2025-06-13 22:24:26 +0300

    style: redisign Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./Header.module.scss";
 import Button from "../ui/Button/Button";
+import { FaHeart } from "react-icons/fa";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -11,24 +12,11 @@ const Header = () => {
         <Button
           variant="primary"
           onClick={() => navigate("/favorites")}
-          startContent="https://img.icons8.com/?size=100&id=10287&format=png&color=FFFFFF"
+          startContent={<FaHeart />}
         >
           Favorites
         </Button>
       </div>
-      <nav className={styles.nav}>
-        <ul className={styles.navLinks}>
-          <li>
-            <Button onClick={() => navigate("/search")}>Search</Button>
-          </li>
-          <li>
-            <Button onClick={() => navigate("/")}>Trends</Button>
-          </li>
-          <li>
-            <Button onClick={() => navigate("/random")}>Random</Button>
-          </li>
-        </ul>
-      </nav>
     </header>
   );
 };
